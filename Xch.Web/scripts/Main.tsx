dc7f25b1974e0bfdbd98ae4c8d342f1a8c2db1b4
Anton Firszov <anton.firszov@sapagroup.com>
2017-11-12 16:57:24 +0100

    added webpack + react + typescript

---
+++ Xch.Web/scripts/Main.tsx
@@ -0,0 +1,12 @@
+﻿import * as React from "react";
+
+import * as ReactDOM from "react-dom";
+
+import { Hello } from "./components/Hello";
+
+
+Hello.renderToElement("root");
+
+export class Main {
+    
+}
---
+++ Xch.Web/scripts/components/Hello.tsx
@@ -0,0 +1,21 @@
+﻿
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+export interface IHelloProps {
+    text: string
+}
+
+export class Hello extends React.Component<IHelloProps, {}> {
+    render() : React.ReactNode {
+        return <div>Hello {this.props.text}!</div>;
+    }
+
+    static renderToElement(elementId: string, text : string = "From React"): void {
+        ReactDOM.render(
+            <Hello text={text}/>,
+            document.getElementById(elementId)
+        );
+    }
+}
+
