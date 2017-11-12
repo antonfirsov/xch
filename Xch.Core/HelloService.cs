a737f998b33c6ec4b6d1f4683de2d11e35ddb5e3
Anton Firszov <anton.firszov@sapagroup.com>
2017-11-12 16:20:09 +0100

    project infrastructure

---
+++ Xch.Core/HelloService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Xch.Core
+{
+    public interface IHelloService
+    {
+        string SayHello();
+    }
+
+    public class HelloService : IHelloService
+    {
+        public string SayHello()
+        {
+            return $"Hello from {this.GetType().FullName}!";
+        }
+    }
+}
---
+++ Xch.Tests/HelloServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xch.Core;
+using Xunit;
+
+namespace Xch.Tests
+{
+    public class HelloServiceTests
+    {
+        [Fact]
+        public void SayHello_ResultIsNotEmpty()
+        {
+            HelloService svc = new HelloService();
+
+            // Act:
+            string hello = svc.SayHello();
+
+            // Assert:
+            Assert.NotNull(hello);
+            Assert.NotEmpty(hello);
+        }
+    }
+}
