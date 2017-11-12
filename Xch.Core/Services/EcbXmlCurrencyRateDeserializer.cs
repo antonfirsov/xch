d2d56ed49ee4e59bbb7451a3b31f250aa63dec94
Anton Firszov <anton.firszov@sapagroup.com>
2017-11-12 18:17:18 +0100

    implmented EcbXmlCurrencyRateDeserializer

--- Xch.Core/Services/EcbXmlCurrencyRateDeserializer.cs
+++ Xch.Core/Services/EcbXmlCurrencyRateDeserializer.cs
@@ -1,15 +1,52 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
 using Xch.Core.Model;
 
 namespace Xch.Core.Services
 {
     public class EcbXmlCurrencyRateDeserializer : ICurrencyRateDeserializer
     {
-        public Task<IEnumerable<CurrencyRatesSnapshot>> DeserializeCurrencyRatesAsync(Stream stream)
+        private static CurrencyRate ConvertRate(XElement element)
         {
-            throw new System.NotImplementedException();
+            string code = (string) element.Attribute("currency");
+            double rate = (double) element.Attribute("rate");
+            return new CurrencyRate(code, rate);
+        }
+
+        private static CurrencyRatesSnapshot ConvertSnapshot(XElement element, XName cubeName)
+        {
+            string dateStr = (string) element.Attribute("time");
+            var date = DateTime.ParseExact(dateStr, "yyyy-MM-dd", CultureInfo.InvariantCulture);
+            var rates = element.Elements(cubeName).Select(ConvertRate);
+            return new CurrencyRatesSnapshot(date, rates);
+        }
+
+        public async Task<IEnumerable<CurrencyRatesSnapshot>> DeserializeCurrencyRatesAsync(Stream stream)
+        {
+            // TODO: Could be optimized with XmlReader
+
+            var reader = new StreamReader(stream);
+            string xmlText = await reader.ReadToEndAsync();
+            XDocument doc = XDocument.Parse(xmlText);
+
+            if (doc.Root == null)
+            {
+                throw new Exception("EcbXmlCurrencyRateDeserializer: invalid xml!");
+            }
+
+            var ns = doc.Root.GetDefaultNamespace();
+            
+            XName cubeName = XName.Get("Cube", ns.NamespaceName);
+            
+            var snapshotElements = doc.Root.Elements(cubeName).Single().Elements(cubeName);
+
+            return snapshotElements.Select(e => ConvertSnapshot(e, cubeName));
         }
     }
 }
--- Xch.Tests/Services/EcbXmlCurrencyRateDeserializerTests.cs
+++ Xch.Tests/Services/EcbXmlCurrencyRateDeserializerTests.cs
@@ -12,8 +12,7 @@ namespace Xch.Tests.Services
 {
     public class EcbXmlCurrencyRateDeserializerTests
     {
-        public const string SampleXml = @"
-<?xml version=""1.0"" encoding=""UTF-8""?>
+        public const string SampleXml = @"<?xml version=""1.0"" encoding=""UTF-8""?>
 
 <gesmes:Envelope xmlns:gesmes=""http://www.gesmes.org/xml/2002-08-01""
                  xmlns=""http://www.ecb.int/vocabulary/2002-08-01/eurofxref"">
@@ -35,18 +34,18 @@ namespace Xch.Tests.Services
       <Cube currency=""PHP"" rate=""59.713"" /><Cube currency=""SGD"" rate=""1.5854"" /><Cube currency=""THB"" rate=""38.61"" />
       <Cube currency=""ZAR"" rate=""16.7585"" />
     </Cube>
-    <Cube time=""2017-11-09"">
-      <Cube currency=""USD"" rate=""1.163"" /><Cube currency=""JPY"" rate=""131.75"" /><Cube currency=""BGN"" rate=""1.9558"" />
-      <Cube currency=""CZK"" rate=""25.528"" /><Cube currency=""DKK"" rate=""7.4422"" /><Cube currency=""GBP"" rate=""0.88633"" />
-      <Cube currency=""HUF"" rate=""312.08"" /><Cube currency=""PLN"" rate=""4.2346"" /><Cube currency=""RON"" rate=""4.64"" />
-      <Cube currency=""SEK"" rate=""9.7355"" /><Cube currency=""CHF"" rate=""1.1589"" /><Cube currency=""NOK"" rate=""9.4535"" />
-      <Cube currency=""HRK"" rate=""7.5385"" /><Cube currency=""RUB"" rate=""69.0726"" /><Cube currency=""TRY"" rate=""4.4962"" />
-      <Cube currency=""AUD"" rate=""1.5144"" /><Cube currency=""BRL"" rate=""3.7762"" /><Cube currency=""CAD"" rate=""1.4779"" />
-      <Cube currency=""CNY"" rate=""7.7193"" /><Cube currency=""HKD"" rate=""9.0701"" /><Cube currency=""IDR"" rate=""15729.57"" />
-      <Cube currency=""ILS"" rate=""4.0877"" /><Cube currency=""INR"" rate=""75.56"" /><Cube currency=""KRW"" rate=""1299.91"" />
-      <Cube currency=""MXN"" rate=""22.2416"" /><Cube currency=""MYR"" rate=""4.879"" /><Cube currency=""NZD"" rate=""1.6715"" />
-      <Cube currency=""PHP"" rate=""59.597"" /><Cube currency=""SGD"" rate=""1.5819"" /><Cube currency=""THB"" rate=""38.495"" />
-      <Cube currency=""ZAR"" rate=""16.5306"" />
+    <Cube time=""2017-09-01"">
+      <Cube currency=""USD"" rate=""1.192"" /><Cube currency=""JPY"" rate=""131.29"" /><Cube currency=""BGN"" rate=""1.9558"" />
+      <Cube currency=""CZK"" rate=""26.077"" /><Cube currency=""DKK"" rate=""7.4378"" /><Cube currency=""GBP"" rate=""0.92075"" />
+      <Cube currency=""HUF"" rate=""305.09"" /><Cube currency=""PLN"" rate=""4.2406"" /><Cube currency=""RON"" rate=""4.5963"" />
+      <Cube currency=""SEK"" rate=""9.4778"" /><Cube currency=""CHF"" rate=""1.1441"" /><Cube currency=""NOK"" rate=""9.2555"" />
+      <Cube currency=""HRK"" rate=""7.4215"" /><Cube currency=""RUB"" rate=""68.8223"" /><Cube currency=""TRY"" rate=""4.0981"" />
+      <Cube currency=""AUD"" rate=""1.5021"" /><Cube currency=""BRL"" rate=""3.7423"" /><Cube currency=""CAD"" rate=""1.483"" />
+      <Cube currency=""CNY"" rate=""7.8185"" /><Cube currency=""HKD"" rate=""9.3272"" /><Cube currency=""IDR"" rate=""15875.38"" />
+      <Cube currency=""ILS"" rate=""4.2599"" /><Cube currency=""INR"" rate=""76.3275"" /><Cube currency=""KRW"" rate=""1335.36"" />
+      <Cube currency=""MXN"" rate=""21.2608"" /><Cube currency=""MYR"" rate=""5.0904"" /><Cube currency=""NZD"" rate=""1.6638"" />
+      <Cube currency=""PHP"" rate=""60.9"" /><Cube currency=""SGD"" rate=""1.6146"" /><Cube currency=""THB"" rate=""39.539"" />
+      <Cube currency=""ZAR"" rate=""15.4185"" />
     </Cube>
   </Cube>
 </gesmes:Envelope>
@@ -59,10 +58,11 @@ namespace Xch.Tests.Services
                 // Arrange:
                 ICurrencyRateDeserializer deserializer = new EcbXmlCurrencyRateDeserializer();
 
-                var stream = GetStreamFromString(SampleXml);
-
-                // Act:
-                return deserializer.DeserializeCurrencyRatesAsync(stream);
+                using (var stream = GetStreamFromString(SampleXml))
+                {
+                    // Act:
+                    return deserializer.DeserializeCurrencyRatesAsync(stream);
+                }
             }
 
             [Fact]
@@ -87,7 +87,7 @@ namespace Xch.Tests.Services
                 var snapshot0 = orderedResult[0];
                 var snapshot1 = orderedResult[1];
 
-                Xunit.Assert.Equal(new DateTime(2017, 11, 9), snapshot0.Date);
+                Xunit.Assert.Equal(new DateTime(2017, 09, 01), snapshot0.Date);
                 Xunit.Assert.Equal(new DateTime(2017, 11, 10), snapshot1.Date);
             }
 
@@ -105,7 +105,7 @@ namespace Xch.Tests.Services
 
                 CurrencyCode[] expectedCodes =
                 {
-                    "USD", "JPY", "BGN", "CZK", "DKK GBP", "HUF", "PLN", "RON", "SEK", "CHF", "NOK", "HRK", "RUB", "TRY",
+                    "USD", "JPY", "BGN", "CZK", "DKK", "GBP", "HUF", "PLN", "RON", "SEK", "CHF", "NOK", "HRK", "RUB", "TRY",
                     "AUD", "BRL", "CAD", "CNY", "HKD", "IDR", "ILS", "INR", "KRW", "MXN", "MYR", "NZD", "PHP", "SGD",
                     "THB", "ZAR"
                 };
@@ -123,10 +123,13 @@ namespace Xch.Tests.Services
                 // Assert:
                 var snapshot0 = result.OrderBy(s => s.Date).First();
 
-                Assert.Equal(1.163, snapshot0["USD"].Rate);
-                Assert.Equal(131.75, snapshot0["JPY"].Rate);
-                Assert.Equal(25.528, snapshot0["CZK"].Rate);
-                Assert.Equal(0.88633, snapshot0["GBP"].Rate);
+                //< Cube currency = ""USD"" rate = ""1.192"" />< Cube currency = ""JPY"" rate = ""131.29"" />< Cube currency = ""BGN"" rate = ""1.9558"" />
+                //< Cube currency = ""CZK"" rate = ""26.077"" />< Cube currency = ""DKK"" rate = ""7.4378"" />< Cube currency = ""GBP"" rate = ""0.92075"" />
+
+                Assert.Equal(1.192, snapshot0["USD"].Rate);
+                Assert.Equal(131.29, snapshot0["JPY"].Rate);
+                Assert.Equal(26.077, snapshot0["CZK"].Rate);
+                Assert.Equal(0.92075, snapshot0["GBP"].Rate);
             }
         }
         
