e2ba6dace4775128d4af89ce034f05be130c2ce5
Anton Firszov <anton.firszov@sapagroup.com>
2017-11-12 19:18:04 +0100

    more CurrencyRateProvider tests

--- Xch.Core/Services/Implementation/CurrencyRateProvider.cs
+++ Xch.Core/Services/Implementation/CurrencyRateProvider.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Xch.Core.Model;
 
@@ -7,18 +8,25 @@ namespace Xch.Core.Services.Implementation
 {
     public class CurrencyRateProvider : ICurrencyRateProvider
     {
+        public string Uri { get; }
         private IBasicHttpWebRequestExecutor _webRequestExecutor;
         private ICurrencyRateDeserializer _deserializer;
 
-        public CurrencyRateProvider(IBasicHttpWebRequestExecutor webRequestExecutor, ICurrencyRateDeserializer deserializer)
+        public CurrencyRateProvider(string uri, IBasicHttpWebRequestExecutor webRequestExecutor, ICurrencyRateDeserializer deserializer)
         {
+            Uri = uri;
             _webRequestExecutor = webRequestExecutor;
             _deserializer = deserializer;
         }
 
-        public Task<CurrencyRatesSnapshot> GetCurrentRatesAsync()
+        public async Task<CurrencyRatesSnapshot> GetCurrentRatesAsync()
         {
-            throw new NotImplementedException();
+            await _webRequestExecutor.ExecuteAsync(Uri);
+            var stream = _webRequestExecutor.GetResponseStream();
+            var rates = await _deserializer.DeserializeCurrencyRatesAsync(stream);
+            
+            // TODO: I was not able to find a proper spec for the XML format. Do we expect an ordered input? Lets go for sure for now and sort it!
+            return rates.OrderBy(r => r.Date).Last();
         }
 
         public Task<IEnumerable<CurrencyRatesSnapshot>> GetHistory(DateTime? minDate, DateTime? maxDate)
